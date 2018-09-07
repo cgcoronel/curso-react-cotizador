7d1c6a50760455f75f45c439b82a1fb019ca138f
Cristian Coronel <thesequencer@hotmail.com>
2018-09-06 22:10:46 -0300

    Update Resultado.js

--- src/components/Resultado.js
+++ src/components/Resultado.js
@@ -4,7 +4,7 @@ import {TransitionGroup, CSSTransition} from 'react-transition-group';
 class Resultado extends Component {
 	render(){
 		const resultado = this.props.resultado;
-		const mensaje = (!resultado) ? 'Elije Marca, año y Tipo de Seguro' : 'El total es; ';
+		const mensaje = (!resultado) ? 'Elije Marca, año y Tipo de Seguro' : 'El total es $ ';
 
 		return (
 			<div className="gran-total">
@@ -14,7 +14,7 @@ class Resultado extends Component {
 						classNames="resultado"
 						key={resultado}
 						timeout={{enter: 500, exit: 500}} >
-						<span>${resultado}.-</span>
+						<span>{resultado}.-</span>
 					</CSSTransition>
 				</TransitionGroup>
 			</div>
